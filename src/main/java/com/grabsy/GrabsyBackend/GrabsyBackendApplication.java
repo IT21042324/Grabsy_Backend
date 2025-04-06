2b57c0d2b2a040e9defe99fdf5d71fec4908e841
IT21042324 <imreezan@gmail.com>
2025-04-06 17:37:44 +0530

    Initialised Project with spring initializer

---
+++ src/main/java/com/grabsy/GrabsyBackend/GrabsyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grabsy.GrabsyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabsyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabsyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grabsy/GrabsyBackend/GrabsyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabsy.GrabsyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabsyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
