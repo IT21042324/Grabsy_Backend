abcf8befc031c4d6133ef02ef6356ea31c8ff482
Sathira-Wijeratne <swije27@gmail.com>
2025-04-09 20:09:32 +0530

    implemented Card model

---
+++ src/main/java/com/grabsy/GrabsyBackend/model/Card.java
@@ -0,0 +1,21 @@
+package com.grabsy.GrabsyBackend.model;
+
+public class Card {
+    private String cardNumber, expiryDate;
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
