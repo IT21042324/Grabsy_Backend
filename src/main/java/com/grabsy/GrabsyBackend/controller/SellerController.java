e6c1ed03123ad89adce9b2055b3702477630bedf
Sathira-Wijeratne <swije27@gmail.com>
2025-04-10 22:31:05 +0530

    initiated creation of seller controller

---
+++ src/main/java/com/grabsy/GrabsyBackend/controller/SellerController.java
@@ -0,0 +1,8 @@
+package com.grabsy.GrabsyBackend.controller;
+
+/**
+ * This class is a controller for the Seller entity, it contains endpoints related to sellers.
+ */
+
+public class SellerController {
+}
