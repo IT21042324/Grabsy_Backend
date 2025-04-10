6b3f42b51b3c8d7191e1eacdd36a397e52aab4e0
Sathira-Wijeratne <swije27@gmail.com>
2025-04-10 22:31:36 +0530

    initiated creation of signer user service

---
+++ src/main/java/com/grabsy/GrabsyBackend/service/SignedUserService.java
@@ -0,0 +1,8 @@
+package com.grabsy.GrabsyBackend.service;
+
+/**
+ * This class is a placeholder for the SignedUserService, it contains business logic related to signed users.
+ */
+
+public class SignedUserService {
+}
