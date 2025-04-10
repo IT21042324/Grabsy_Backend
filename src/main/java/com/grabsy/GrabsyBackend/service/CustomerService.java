4ace84145aa2af228634a9a146b4290677688326
Sathira-Wijeratne <swije27@gmail.com>
2025-04-10 19:59:52 +0530

    initiated Customer service

---
+++ src/main/java/com/grabsy/GrabsyBackend/service/CustomerService.java
@@ -0,0 +1,20 @@
+package com.grabsy.GrabsyBackend.service;
+
+import com.grabsy.GrabsyBackend.Repository.CustomerRepository;
+
+/**
+ * This class is a service for the Customer entity, it contains business logic related to customers.
+ */
+
+// TODO
+
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    // constructor
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+
+}
