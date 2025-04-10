7d0ccb2b06725660aaa7668365d8f6cf4b2f7a87
Sathira-Wijeratne <swije27@gmail.com>
2025-04-10 22:31:15 +0530

    initiated creation of seller service

---
+++ src/main/java/com/grabsy/GrabsyBackend/service/SellerService.java
@@ -0,0 +1,8 @@
+package com.grabsy.GrabsyBackend.service;
+
+/**
+ * This class is a service for the Seller entity, it contains business logic related to sellers.
+ */
+
+public class SellerService {
+}
