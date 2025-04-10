d465f3462728db54fe0af5576278e9d6ee014f28
Sathira-Wijeratne <swije27@gmail.com>
2025-04-10 22:34:34 +0530

    modified Product entity to adjust for removal of lombok

--- src/main/java/com/grabsy/GrabsyBackend/Entity/Product.java
+++ src/main/java/com/grabsy/GrabsyBackend/Entity/Product.java
@@ -1,12 +1,8 @@
 package com.grabsy.GrabsyBackend.Entity;
 
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
-@Getter
-@Setter
 public class Product {
     @Id
     private String Id;
@@ -40,6 +36,94 @@ public class Product {
         this.store = store;
         this.ratings = ratings;
         this.itemsSold = itemsSold;
+        this.offer = offer;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Rendition getRendition() {
+        return rendition;
+    }
+
+    public void setRendition(Rendition rendition) {
+        this.rendition = rendition;
+    }
+
+    public Long getItemCount() {
+        return itemCount;
+    }
+
+    public void setItemCount(Long itemCount) {
+        this.itemCount = itemCount;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public float getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(float ratings) {
+        this.ratings = ratings;
+    }
+
+    public Long getItemsSold() {
+        return itemsSold;
+    }
+
+    public void setItemsSold(Long itemsSold) {
+        this.itemsSold = itemsSold;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
         this.offer = offer;
     }
 }
