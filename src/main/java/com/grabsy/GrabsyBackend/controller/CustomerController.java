d3ae80432679b179c474b8d869ea6595abdff9bf
Sathira-Wijeratne <swije27@gmail.com>
2025-04-17 12:08:27 +0530

    removed unused imports

--- src/main/java/com/grabsy/GrabsyBackend/controller/CustomerController.java
+++ src/main/java/com/grabsy/GrabsyBackend/controller/CustomerController.java
@@ -4,8 +4,6 @@ import com.grabsy.GrabsyBackend.service.CustomerService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.parser.Entity;
-
 /**
  * This class is a controller for the Customer entity, it handles HTTP requests related to customers.
  */
