b8efeb46bae446d046425f0f8454379944bad32c
Sathira-Wijeratne <swije27@gmail.com>
2025-04-17 12:07:45 +0530

    removed browseCatalog() functionality to ensure separation of concerns

--- src/main/java/com/grabsy/GrabsyBackend/controller/CustomerController.java
+++ src/main/java/com/grabsy/GrabsyBackend/controller/CustomerController.java
@@ -1,8 +1,21 @@
 package com.grabsy.GrabsyBackend.controller;
 
+import com.grabsy.GrabsyBackend.service.CustomerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.parser.Entity;
+
 /**
  * This class is a controller for the Customer entity, it handles HTTP requests related to customers.
  */
 
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 }
--- src/main/java/com/grabsy/GrabsyBackend/service/CustomerService.java
+++ src/main/java/com/grabsy/GrabsyBackend/service/CustomerService.java
@@ -1,11 +1,9 @@
 package com.grabsy.GrabsyBackend.service;
 
-import com.grabsy.GrabsyBackend.entity.Product;
 import com.grabsy.GrabsyBackend.repository.CustomerRepository;
 import com.grabsy.GrabsyBackend.repository.ProductRepository;
 import com.grabsy.GrabsyBackend.assembler.ProductModelAssembler;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
+import org.springframework.stereotype.Service;
 
 /**
  * This class is a service for the Customer entity, it contains business logic related to customers.
@@ -13,25 +11,12 @@ import org.springframework.hateoas.EntityModel;
 
 // TODO
 
+@Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
-    private final ProductRepository productRepository;
-    private final ProductModelAssembler productAssembler;
 
     // constructor
     public CustomerService(CustomerRepository customerRepository, ProductRepository productRepository, ProductModelAssembler productAssembler) {
         this.customerRepository = customerRepository;
-        this.productRepository = productRepository;
-        this.productAssembler = productAssembler;
-    }
-
-    // methods
-
-    /**
-     * This method retrieves all products from the catalog and converts them to a CollectionModel<EntityModel<Product>> object.
-     * @return the CollectionModel<EntityModel<Product>> object
-     */
-    public CollectionModel<EntityModel<Product>> browseCatalog(){
-        return productAssembler.toCollectionModel(productRepository.findAll());
     }
 }
