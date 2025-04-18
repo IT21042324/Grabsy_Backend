d28dcf9fc7a4c7cc9f74b26d42e47feb90e7965e
Sathira-Wijeratne <swije27@gmail.com>
2025-04-18 20:09:51 +0530

    removed ProductModelAssembler.java

--- src/main/java/com/grabsy/GrabsyBackend/service/CustomerService.java
+++ src/main/java/com/grabsy/GrabsyBackend/service/CustomerService.java
@@ -1,8 +1,6 @@
 package com.grabsy.GrabsyBackend.service;
 
 import com.grabsy.GrabsyBackend.repository.CustomerRepository;
-import com.grabsy.GrabsyBackend.repository.ProductRepository;
-import com.grabsy.GrabsyBackend.assembler.ProductModelAssembler;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,7 +14,7 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
 
     // constructor
-    public CustomerService(CustomerRepository customerRepository, ProductRepository productRepository, ProductModelAssembler productAssembler) {
+    public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 }
