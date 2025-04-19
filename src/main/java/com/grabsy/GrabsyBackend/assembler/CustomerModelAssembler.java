8262466654981e5211bce44877d46e6216d026e9
Sathira-Wijeratne <swije27@gmail.com>
2025-04-19 20:46:34 +0530

    Implement CustomerModelAssembler
    Converts from Customer EntityModel object

--- src/main/java/com/grabsy/GrabsyBackend/assembler/CustomerModelAssembler.java
+++ src/main/java/com/grabsy/GrabsyBackend/assembler/CustomerModelAssembler.java
@@ -1,12 +1,30 @@
 package com.grabsy.GrabsyBackend.assembler;
 
+import com.grabsy.GrabsyBackend.controller.CustomerController;
 import com.grabsy.GrabsyBackend.entity.users.Customer;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+/**
+ * This class is responsible for converting Customer entities into EntityModel objects.
+ */
+
+@Component
 public class CustomerModelAssembler implements RepresentationModelAssembler<Customer, EntityModel<Customer>> {
     @Override
     public EntityModel<Customer> toModel(Customer customer) {
-        return null; // TODO : Implement this properly
+        return EntityModel.of(customer,
+                linkTo(methodOn(CustomerController.class).getCustomerById(customer.getUserId())).withSelfRel(),
+                linkTo(methodOn(CustomerController.class).findAllCustomers()).withRel("customers"));
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Customer>> toCollectionModel(Iterable<? extends Customer> entities) {
+        return RepresentationModelAssembler.super.toCollectionModel(entities);
     }
 }
