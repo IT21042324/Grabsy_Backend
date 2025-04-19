5e1e0d0ee2bd25e796d3f7413d1b9a1be8267dd0
Sathira-Wijeratne <swije27@gmail.com>
2025-04-19 20:27:30 +0530

    add UserRole enum for role-based access control

---
+++ src/main/java/com/grabsy/GrabsyBackend/constant/UserRole.java
@@ -0,0 +1,11 @@
+package com.grabsy.GrabsyBackend.constant;
+
+/**
+ * This enum represents the different roles a user can have in the system.
+ */
+
+public enum UserRole {
+    CUSTOMER,
+    SELLER,
+    ADMIN
+}
