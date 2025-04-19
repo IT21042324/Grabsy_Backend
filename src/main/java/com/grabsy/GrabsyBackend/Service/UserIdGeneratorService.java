49011fe0b0517e580cef281364812c74bf8f6476
Sathira-Wijeratne <swije27@gmail.com>
2025-04-19 12:10:35 +0530

    implemented logic to generate user id

---
+++ src/main/java/com/grabsy/GrabsyBackend/Service/UserIdGeneratorService.java
@@ -0,0 +1,31 @@
+package com.grabsy.GrabsyBackend.service;
+
+import com.grabsy.GrabsyBackend.entity.UserIdCounter;
+import com.grabsy.GrabsyBackend.repository.UserIdCounterRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserIdGeneratorService {
+    private final UserIdCounterRepository userIdCounterRepository;
+
+    public UserIdGeneratorService(UserIdCounterRepository userIdCounterRepository){
+        this.userIdCounterRepository = userIdCounterRepository;
+    }
+
+    // TODO : Handle exceptions
+    /**
+     * This method generates a new user id based on the role of the user.
+     * @param role the role of the user
+     * @return a new user id
+     */
+    public String generateUserId(String role){
+        UserIdCounter counter = userIdCounterRepository.findById(role.toUpperCase())
+                .orElseGet(() -> new UserIdCounter(role.toUpperCase(), 0));
+
+        int newCounterValue = counter.getCounter() + 1;
+        counter.setCounter(newCounterValue);
+        userIdCounterRepository.save(counter);
+
+        return role.toUpperCase().charAt(0) + String.format("%03d", newCounterValue);
+    }
+}
