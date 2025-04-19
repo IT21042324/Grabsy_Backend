81940918626d06692fa0380128e7f6c5d4d9e574
Sathira-Wijeratne <swije27@gmail.com>
2025-04-19 12:09:15 +0530

    implemented entity to store latest userIds

---
+++ src/main/java/com/grabsy/GrabsyBackend/entity/UserIdCounter.java
@@ -0,0 +1,34 @@
+package com.grabsy.GrabsyBackend.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * This class is used to generate the sequence number for the user id rather than using the default MongoDB id.
+ */
+
+@Document(collection = "userIdCounter")
+public class UserIdCounter {
+    private String userRole;
+    private int counter;
+
+    public UserIdCounter(String userRole, int counter) {
+        this.userRole = userRole;
+        this.counter = counter;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
