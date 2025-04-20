8ea86ab433542d403ba0444f0ab83a79d3e2cb76
Sathira-Wijeratne <swije27@gmail.com>
2025-04-20 12:37:30 +0530

    Remove registerCustomer controller method

--- src/main/java/com/grabsy/GrabsyBackend/controller/CustomerController.java
+++ src/main/java/com/grabsy/GrabsyBackend/controller/CustomerController.java
@@ -1,15 +1,12 @@
 package com.grabsy.GrabsyBackend.controller;
 
 import com.grabsy.GrabsyBackend.assembler.CustomerModelAssembler;
-import com.grabsy.GrabsyBackend.dto.SignedUserDto;
 import com.grabsy.GrabsyBackend.entity.users.Customer;
 import com.grabsy.GrabsyBackend.exception.CustomerNotFoundException;
 import com.grabsy.GrabsyBackend.repository.CustomerRepository;
 import com.grabsy.GrabsyBackend.service.CustomerService;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -19,13 +16,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
-    private final CustomerService customerService;
     private final CustomerModelAssembler customerModelAssembler;
     private final CustomerRepository customerRepository;
 
     public CustomerController(CustomerService customerService, CustomerModelAssembler customerModelAssembler,
                               CustomerRepository customerRepository) {
-        this.customerService = customerService;
         this.customerModelAssembler = customerModelAssembler;
         this.customerRepository = customerRepository;
     }
@@ -50,20 +45,5 @@ public class CustomerController {
                 .orElseThrow(() -> new CustomerNotFoundException(userId));
 
         return customerModelAssembler.toModel(customer);
-    }
-
-    /**
-     * This method handles the HTTP POST request to register a new customer.
-     * @param signedUserDto The DTO containing the customer's information.
-     * @return A ResponseEntity containing the created customer wrapped in an EntityModel.
-     * @throws IllegalAccessException If the email format is invalid.
-     */
-    @PostMapping("/register")
-    public ResponseEntity<EntityModel<Customer>> registerCustomer(@RequestBody SignedUserDto signedUserDto) throws IllegalAccessException {
-        Customer customer = customerService.registerCustomer(signedUserDto);
-
-        EntityModel<Customer> entityModel = customerModelAssembler.toModel(customerRepository.save(customer));
-
-        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 }
