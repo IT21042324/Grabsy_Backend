30d42f9235a97513d2851773167e97e695966f97
Sathira-Wijeratne <swije27@gmail.com>
2025-04-20 19:39:47 +0530

    Add meaningful comments to methods

--- src/main/java/com/grabsy/GrabsyBackend/assembler/CustomerModelAssembler.java
+++ src/main/java/com/grabsy/GrabsyBackend/assembler/CustomerModelAssembler.java
@@ -16,6 +16,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 public class CustomerModelAssembler implements RepresentationModelAssembler<Customer, EntityModel<Customer>> {
+
+    /**
+     * This method converts a Customer entity into an EntityModel object.
+     * @param customer The Customer entity to convert.
+     * @return An EntityModel containing the Customer entity and its links.
+     */
     @Override
     public EntityModel<Customer> toModel(Customer customer) {
         return EntityModel.of(customer,
@@ -23,6 +29,11 @@ public class CustomerModelAssembler implements RepresentationModelAssembler<Cust
                 linkTo(methodOn(CustomerController.class).findAllCustomers()).withRel("customers"));
     }
 
+    /**
+     * This method converts a collection of Customer entities into a CollectionModel object.
+     * @param entities The collection of Customer entities to convert.
+     * @return A CollectionModel containing the EntityModels of the Customer entities.
+     */
     @Override
     public CollectionModel<EntityModel<Customer>> toCollectionModel(Iterable<? extends Customer> entities) {
         return RepresentationModelAssembler.super.toCollectionModel(entities);
