bd90a953c9e6219ad68e30818dbeaf40f1cda890
Sathira-Wijeratne <swije27@gmail.com>
2025-04-21 10:22:12 +0530

    Implement UserValidationService for user input validation
    - Add methods to validate phone numbers, passwords, and email formats
    - Ensure email uniqueness based on user roles

---
+++ src/main/java/com/grabsy/GrabsyBackend/Service/UnsignedUserService.java
@@ -0,0 +1,12 @@
+package com.grabsy.GrabsyBackend.service;
+
+import com.grabsy.GrabsyBackend.domain.SignedUser;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnsignedUserService {
+    // public SignedUser register(){
+    //
+    // }
+
+}
