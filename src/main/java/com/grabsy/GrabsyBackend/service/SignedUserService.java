69b8d1da660e46d304705a217100518f3be869b0
Sathira-Wijeratne <swije27@gmail.com>
2025-04-21 12:26:27 +0530

    Add generic getUserById method in SignedUserService with custom exception
    - Implement a reusable generic method to fetch entities by ID

--- src/main/java/com/grabsy/GrabsyBackend/service/SignedUserService.java
+++ src/main/java/com/grabsy/GrabsyBackend/service/SignedUserService.java
@@ -1,8 +1,15 @@
 package com.grabsy.GrabsyBackend.service;
 
+import com.grabsy.GrabsyBackend.exception.UserNotFoundException;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 /**
  * This class is a placeholder for the SignedUserService, it contains business logic related to signed users.
  */
 
-public class SignedUserService {
+public abstract class SignedUserService {
+    protected <T> T getUserById(String userId, MongoRepository<T, String> repository){
+        return repository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+    }
 }
