c3eed5e3a0a8bec877868e552784662fc738a7de
Sathira-Wijeratne <swije27@gmail.com>
2025-04-22 12:18:42 +0530

    Add loggers

--- src/main/java/com/grabsy/GrabsyBackend/Service/user/SignedUserService.java
+++ src/main/java/com/grabsy/GrabsyBackend/Service/user/SignedUserService.java
@@ -1,8 +1,10 @@
-package com.grabsy.GrabsyBackend.service;
+package com.grabsy.GrabsyBackend.service.user;
 
-import com.grabsy.GrabsyBackend.exception.UserDeletionException;
-import com.grabsy.GrabsyBackend.exception.UserFetchException;
-import com.grabsy.GrabsyBackend.exception.UserNotFoundException;
+import com.grabsy.GrabsyBackend.exception.user.UserDeletionException;
+import com.grabsy.GrabsyBackend.exception.user.UserFetchException;
+import com.grabsy.GrabsyBackend.exception.user.UserNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -13,15 +15,19 @@ import java.util.List;
  */
 
 public abstract class SignedUserService {
+    private static final Logger log = LoggerFactory.getLogger(SignedUserService.class);
 
     protected <T> T getUserById(String userId, MongoRepository<T, String> repository){
         try {
             if (!repository.existsById(userId)){
+                log.error("User with id: {} could not be found", userId);
                 throw new UserNotFoundException("User with id: " + userId + " could not be found");
             }
-            return repository.findById(userId)
+
+            repository.findById(userId)
                     .orElseThrow(() -> new UserNotFoundException("User with id: " + userId + " could not be found"));
         } catch (DataAccessException e) {
+            log.error("Error fetching user with id: {}", userId, e);
             throw new UserFetchException("User with id: "  + userId + " could not be fetched", e);
         }
     }
@@ -30,6 +36,7 @@ public abstract class SignedUserService {
         try{
             return repository.findAll();
         } catch (DataAccessException e){
+            log.error("Error fetching users from repository", e);
             throw new UserFetchException("Unable to fetch users from repository", e);
         }
     }
@@ -37,11 +44,13 @@ public abstract class SignedUserService {
     protected <T> void deleteUserById(String userId, MongoRepository<T, String> repository){
         try {
             if(!repository.existsById(userId)){
+                log.error("User with id: {} could not be found", userId);
                 throw new UserNotFoundException("User with id: " + userId + " could not be found");
             }
 
             repository.deleteById(userId);
         } catch (DataAccessException e) {
+            log.error("Error deleting user with id: {}", userId, e);
             throw new UserDeletionException("User with id : " + userId + " could not be deleted", e);
         }
     }
