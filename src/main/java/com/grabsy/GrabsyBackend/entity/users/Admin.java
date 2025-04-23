87c4eee429406ca24938e8cbf15079abd0d83154
Sathira-Wijeratne <swije27@gmail.com>
2025-04-23 21:45:21 +0530

    Add constructor

--- src/main/java/com/grabsy/GrabsyBackend/entity/users/Admin.java
+++ src/main/java/com/grabsy/GrabsyBackend/entity/users/Admin.java
@@ -10,5 +10,5 @@ import org.springframework.data.mongodb.core.mapping.Document;
 // TODO : Is an admin class necessary since it doesn't seem to have it's own unique variables?
 @Document(collection = "admins")
 public class Admin extends SignedUser {
-
+    public Admin(){}
 }
