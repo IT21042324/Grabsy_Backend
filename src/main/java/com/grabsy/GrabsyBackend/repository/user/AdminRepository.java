fdc9b0158b8bdcc3cfbda53a62ae248af15f43c0
Sathira-Wijeratne <swije27@gmail.com>
2025-04-23 21:51:01 +0530

    Fix bug in AdminRepository primary key type
    - Change primary key type from Long to String

--- src/main/java/com/grabsy/GrabsyBackend/repository/user/AdminRepository.java
+++ src/main/java/com/grabsy/GrabsyBackend/repository/user/AdminRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * This interface is a repository for the Admin entity, it extends MongoRepository to provide CRUD operations.
  */
 
-public interface AdminRepository extends MongoRepository<Admin, Long>, EmailRepository {
+public interface AdminRepository extends MongoRepository<Admin, String>, EmailRepository {
     boolean existsByEmail(String email);
 }
