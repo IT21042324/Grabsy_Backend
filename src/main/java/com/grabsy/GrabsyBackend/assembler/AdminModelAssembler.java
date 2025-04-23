791c320a64752c3ed8ceeeac172d6b6709e83e86
Sathira-Wijeratne <swije27@gmail.com>
2025-04-23 21:51:39 +0530

    Add AdminModelAssembler for HATEOAS support
    - Implement RepresentationModelAssembler for Admin entity
    - Provide self and collection links for Admin resources

---
+++ src/main/java/com/grabsy/GrabsyBackend/assembler/AdminModelAssembler.java
@@ -0,0 +1,26 @@
+package com.grabsy.GrabsyBackend.assembler;
+
+import com.grabsy.GrabsyBackend.controller.AdminController;
+import com.grabsy.GrabsyBackend.entity.users.Admin;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class AdminModelAssembler implements RepresentationModelAssembler<Admin, EntityModel<Admin>> {
+    @Override
+    public EntityModel<Admin> toModel(Admin admin) {
+        return EntityModel.of(admin,
+                linkTo(methodOn(AdminController.class).getAdminById(admin.getUserId())).withSelfRel(),
+                linkTo(methodOn(AdminController.class).getAllAdmins()).withRel("admins"));
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Admin>> toCollectionModel(Iterable<? extends Admin> entities) {
+        return RepresentationModelAssembler.super.toCollectionModel(entities);
+    }
+}
