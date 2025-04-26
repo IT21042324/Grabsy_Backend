d75423c506e2a6d9fb9189a173513bcb8bcb6c96
Sathira-Wijeratne <swije27@gmail.com>
2025-04-26 13:04:53 +0530

    Move files to a different package for better organization

--- src/main/java/com/grabsy/GrabsyBackend/exception/user/attributes/InvalidUserIdException.java
+++ src/main/java/com/grabsy/GrabsyBackend/exception/user/attributes/InvalidUserIdException.java
@@ -1,7 +1,5 @@
 package com.grabsy.GrabsyBackend.exception.user.attributes;
 
-import com.grabsy.GrabsyBackend.exception.user.ValidationException;
-
 public class InvalidUserIdException extends ValidationException {
     public InvalidUserIdException(String message) {
         super(message);
