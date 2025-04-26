858b1ddbd1196733699139a183779eb2270657a0
Sathira-Wijeratne <swije27@gmail.com>
2025-04-26 14:06:52 +0530

    refactor: Update method names for clarity and modify `generateUserId` signature
    - Renamed methods to better reflect their functionality for improved readability and maintainability.
    - Updated the `generateUserId` method to accept `UserRole` type instead of `String` for better type safety and alignment with domain logic.

--- src/main/java/com/grabsy/GrabsyBackend/Service/user/SellerService.java
+++ src/main/java/com/grabsy/GrabsyBackend/Service/user/SellerService.java
@@ -20,28 +20,31 @@ import java.util.List;
  * This class is a service for the Seller entity, it contains business logic related to sellers.
  */
 @Service
-public class SellerService extends SignedUserService {
+public class SellerService{
     private final SellerRepository sellerRepository;
     private final SecurityService securityService;
-    private final UserValidationService userValidationService;
     private final UserIdGeneratorService userIdGeneratorService;
     private static final Logger log = LoggerFactory.getLogger(SellerService.class);
+    private final SignedUserService signedUserService;
+    private final UserValidationService userValidationService;
 
     public SellerService(SellerRepository sellerRepository, SecurityService securityService,
-                         UserValidationService userValidationService, UserIdGeneratorService userIdGeneratorService) {
+                         UserIdGeneratorService userIdGeneratorService, SignedUserService signedUserService,
+                         UserValidationService userValidationService) {
         this.sellerRepository = sellerRepository;
         this.securityService = securityService;
-        this.userValidationService = userValidationService;
         this.userIdGeneratorService = userIdGeneratorService;
+        this.signedUserService = signedUserService;
+        this.userValidationService = userValidationService;
     }
 
     public Seller getSellerById(String userId) {
-        return getUserById(userId, sellerRepository);
+        return signedUserService.getUserByIdOrThrow(userId, sellerRepository);
     }
 
     public List<Seller> findAllSellers() {
         try {
-            return getAllUsersByRole(sellerRepository);
+            return signedUserService.getAllUsersByRoleOrThrow(sellerRepository);
         } catch (Exception e) {
             log.error("Error fetching all sellers", e);
             throw new UserFetchException("Unable to fetch all sellers", e);
@@ -50,7 +53,7 @@ public class SellerService extends SignedUserService {
 
     public void removeSellerById(String userId){
         try {
-            deleteUserById(userId, sellerRepository);
+            signedUserService.deleteUserByIdOrThrow(userId, sellerRepository);
         } catch (Exception e) {
             log.error("Error deleting seller with id: {}", userId, e);
             throw new UserDeletionException("Unable to delete seller with id: " + userId, e);
@@ -67,7 +70,7 @@ public class SellerService extends SignedUserService {
         // Map DTO to Seller Entity
         Seller seller =  new Seller();
         seller.setUserRole(String.valueOf(UserRole.SELLER));
-        seller.setUserId(userIdGeneratorService.generateUserId(seller.getUserRole()));
+        seller.setUserId(userIdGeneratorService.generateUserId(UserRole.SELLER));
         seller.setName(sellerDto.getName());
         seller.setEmail(sellerDto.getEmail());
         seller.setPasswordHash(securityService.hashPassword(sellerDto.getPassword()));
