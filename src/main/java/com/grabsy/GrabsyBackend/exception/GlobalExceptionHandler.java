6c5899cd845dbbb7c239ab983231ed59ac673360
Sathira-Wijeratne <swije27@gmail.com>
2025-04-26 14:20:43 +0530

    feat: Add exception handling methods in `GlobalExceptionHandler`
    - Added `handleHttpMessageNotReadableException` to handle invalid input payloads with descriptive error messages.
    - Added `handleDataAccessException` to handle any uncaught database-related errors with appropriate HTTP status.
    - Added `handleGenericException` to handle unexpected errors with a generic response.

--- src/main/java/com/grabsy/GrabsyBackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/grabsy/GrabsyBackend/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.grabsy.GrabsyBackend.exception;
 
 
 import com.grabsy.GrabsyBackend.exception.user.UserException;
-import com.grabsy.GrabsyBackend.exception.user.ValidationException;
+import com.grabsy.GrabsyBackend.exception.user.attributes.ValidationException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +24,21 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(ValidationException.class)
   public ResponseEntity<String> handleValidationException(ValidationException ex){
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+    String errorMessage = "Invalid input. Please check your request payload. For userRole, allowed values are: CUSTOMER, SELLER, ADMIN. Error details: " + ex.getMessage();
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+  }
+
+  @ExceptionHandler(DataAccessException.class)
+  public ResponseEntity<String> handleDataAccessException(DataAccessException ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database error occurred");
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleGenericException(Exception ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred");
   }
 }
