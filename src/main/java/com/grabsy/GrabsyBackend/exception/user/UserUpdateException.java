3ec7006d8fbfcd40c012a30fa53bc2b4839f920b
Sathira-Wijeratne <swije27@gmail.com>
2025-04-26 13:53:55 +0530

    Add constructor for UserUpdateException with message parameter
    -Added a new constructor to UserUpdateException to initialize the exception with a custom message.
    -Enhances flexibility in exception handling by allowing detailed error messages.

--- src/main/java/com/grabsy/GrabsyBackend/exception/user/UserUpdateException.java
+++ src/main/java/com/grabsy/GrabsyBackend/exception/user/UserUpdateException.java
@@ -1,6 +1,10 @@
 package com.grabsy.GrabsyBackend.exception.user;
 
 public class UserUpdateException extends UserException {
+    public UserUpdateException(String message){
+        super(message);
+    }
+
     public UserUpdateException(String message, Throwable cause) {
         super(message, cause);
     }
