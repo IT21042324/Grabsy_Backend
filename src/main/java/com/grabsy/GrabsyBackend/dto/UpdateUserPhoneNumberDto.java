21e19571fa87546735cebe451232cdd2740112ce
Sathira-Wijeratne <swije27@gmail.com>
2025-04-28 12:55:20 +0530

    feat: add UserRole attribute and getters and setters for dto variables

--- src/main/java/com/grabsy/GrabsyBackend/dto/UpdateUserPhoneNumberDto.java
+++ src/main/java/com/grabsy/GrabsyBackend/dto/UpdateUserPhoneNumberDto.java
@@ -1,5 +1,32 @@
 package com.grabsy.GrabsyBackend.dto;
 
+import com.grabsy.GrabsyBackend.constant.UserRole;
+
 public class UpdateUserPhoneNumberDto {
     private String userId, newPhoneNumber;
+    private UserRole userRole;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNewPhoneNumber() {
+        return newPhoneNumber;
+    }
+
+    public void setNewPhoneNumber(String newPhoneNumber) {
+        this.newPhoneNumber = newPhoneNumber;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
 }
